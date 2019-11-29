7a7edc853f3b3d45293504ab87c3435df681c661
Mikael <mikaelpoe@gmail.com>
2019-11-29 17:46:53 -0300

    Updated model and database
    Updated model and database for users to have all information necessary plus a few extras

--- api/api/models.py
+++ api/api/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import FileExtensionValidator
+
+def user_file_directory_path(instance, filename):
+    return 'website/files/user_{0}/{1}'.format(instance.user.id, filename)
 
 class User(AbstractUser):
 
@@ -20,3 +24,44 @@ class User(AbstractUser):
         null=False,
         blank=False
     )
+
+    username = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False,
+        unique = True
+    )
+
+    telephone = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    cpf = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False
+    )
+
+    curriculum = models.FileField(
+        upload_to=user_file_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
+        null = True,
+        blank = True
+    )
+
+    deleteDate = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True
+    )
+
+    isDeleted = models.BooleanField(
+        default = False,
+        null = False,
+        blank = False
+    )
+
+    objects = models.Manager()
